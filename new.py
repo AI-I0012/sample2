86d5b2c24dd00167b6a608136c2e4d4aa1668aa2
AI-I0012 <rohith@aggregateintelligence.in>
2023-09-14 12:25:39 +0530

    newly added

--- new.py
+++ new.py
@@ -1,1 +1,22 @@
 print("Welcome guys and hello everyone")
+from random import randint
+print(f'The random number is {randint(0,100)}')
+mylist=[1,2,3,4,5]
+for i in mylist :
+    if(i%2!=0) :
+        pass
+    else:
+        print(f'it is even {i}')
+i=0
+while i<len(mylist):
+    if(mylist[i]%2==0):
+        print('it is even')
+    else:
+        pass
+    i+=1
+print(range(0,100))
+
+def sum_of_nums(a,b):
+    return a+b
+print(sum_of_nums(10,20.0))
+print('new from today')
